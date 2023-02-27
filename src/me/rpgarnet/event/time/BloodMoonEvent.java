8e158e80127db56b8062898afcc019de9aefa720
MattiaBiancini <m.biancini1@campus.unimib.it>
2023-02-27 14:11:30 +0100

    Bugfixes Bloodmoon

--- src/me/rpgarnet/event/time/BloodMoonEvent.java
+++ src/me/rpgarnet/event/time/BloodMoonEvent.java
@@ -21,12 +21,12 @@ public class BloodMoonEvent implements Listener {
 			if(entity instanceof Monster) {
 				
 				Monster monster = (Monster) entity;
-				monster.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE).setBaseValue(monster.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE).getBaseValue() * 3);
-				monster.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(monster.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).getBaseValue() * 1.5);
-				monster.getAttribute(Attribute.GENERIC_ARMOR).setBaseValue(10);
-				monster.getAttribute(Attribute.GENERIC_FOLLOW_RANGE).setBaseValue(monster.getAttribute(Attribute.GENERIC_FOLLOW_RANGE).getBaseValue() * 3);
+				monster.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE).setBaseValue(monster.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE).getBaseValue() * 5);
+				monster.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(monster.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).getBaseValue() * 2);
+				monster.getAttribute(Attribute.GENERIC_ARMOR).setBaseValue(20);
+				monster.getAttribute(Attribute.GENERIC_FOLLOW_RANGE).setBaseValue(monster.getAttribute(Attribute.GENERIC_FOLLOW_RANGE).getBaseValue() * 10);
 				monster.getAttribute(Attribute.GENERIC_KNOCKBACK_RESISTANCE).setBaseValue(0.6);
-				monster.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(monster.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue() * 1.5);
+				monster.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(monster.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue() * 3);
 				
 				if(entity instanceof Zombie) {
 					monster.getAttribute(Attribute.ZOMBIE_SPAWN_REINFORCEMENTS).setBaseValue(0.3);
